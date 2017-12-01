697b3335023c13ec1dfbd1f80fa550dadcbf7259
alex <alex@staticlibs.net>
2017-12-01 00:21:20 +0000

    browser compat

--- tests.js
+++ tests.js
@@ -1,8 +1,8 @@
-define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
 var assert = require("assert"),
-    boolbase = require("boolbase/");
+    boolbase = require("boolbase");
 
 assert.strictEqual(boolbase.trueFunc(), true, "should evaluate to true");
 assert.strictEqual(boolbase.falseFunc(), false, "should evaluate to false");
 
-return module.exports;});
+require = requireOrig;});
